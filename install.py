8743d0f666bd90bdf366fb9a8db7cad62497a0f3
brantje <brantje@gmail.com>
2025-04-18 11:17:15 +0000

    Enable repository cloning in install.py by uncommenting the relevant code block, allowing for automatic cloning of specified repositories if they do not already exist.

--- install.py
+++ install.py
@@ -14,7 +14,7 @@ repos = {
     'comfyui_controlnet_aux': 'https://github.com/Fannovel16/comfyui_controlnet_aux'
 }
 
-# for name, url in repos.items():
-#     target_dir = os.path.join(parent_dir, name)
-#     if not os.path.exists(target_dir):
-#         subprocess.run(['git', 'clone', url, target_dir], check=True)
+for name, url in repos.items():
+    target_dir = os.path.join(parent_dir, name)
+    if not os.path.exists(target_dir):
+        subprocess.run(['git', 'clone', url, target_dir], check=True)
